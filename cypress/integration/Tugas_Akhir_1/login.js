e39083c9e74242f990a469ad6d147adaacbec79e
myrizkyprasetya <ifharizky28@gmail.com>
2021-09-03 20:21:21 +0700

    proses login

--- cypress/integration/Tugas_Akhir_1/login.js
+++ cypress/integration/Tugas_Akhir_1/login.js
@@ -2,5 +2,11 @@ describe('proses login', ()=>{
     it('tc-login', ()=>{
         cy.visit('https://admin.pkh.dojobox.id/')
 
+        cy.get('#input-email').type('e@dojobox.id', {force:true}).should('have.value', 'e@dojobox.id')
+
+        cy.get('#input-password').type('admin').should('have.value', 'admin')
+
+        cy.get('#btn-login').should('have.id', 'btn-login').click()
+
     })
 })
